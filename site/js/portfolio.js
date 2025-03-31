9fd3418cb35035be377d04253807f137df657d82
corentinthebaud <corentin.thebaud44@gmail.com>
2025-03-31 13:02:45 +0200

    Java script c'est dur

---
+++ site/js/portfolio.js
@@ -0,0 +1,9 @@
+const buttonColor = document.querySelector("#clickbutton");
+const firstName = document.querySelector("#firstname");
+
+buttonColor.addEventListener("click", () => {
+    alert(prompt("What's your name ?"));
+    
+
+    
+});
